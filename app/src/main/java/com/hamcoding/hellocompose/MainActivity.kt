67f9fecd3c0ceda07369b4160ce85b3e4c431e58
이경은 <dientes8520@gmail.com>
2023-11-06 18:08:35 +0900

    learn: Button Component

--- app/src/main/java/com/hamcoding/hellocompose/MainActivity.kt
+++ app/src/main/java/com/hamcoding/hellocompose/MainActivity.kt
@@ -1,10 +1,23 @@
 package com.hamcoding.hellocompose
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,39 +38,54 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HelloComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ButtonExample(onButtonClicked = {
+                    Toast.makeText(this, "Send Clicked.", Toast.LENGTH_SHORT).show()
+                })
             }
         }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HelloComposeTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun ComposePreview() {
+        HelloComposeTheme {
+            ButtonExample(onButtonClicked = {})
+        }
     }
-}
 
-/** 텍스트 */
-@Composable
-fun Greeting(name: String) {
-    Text(
-        modifier = Modifier.width(300.dp),
-        text = "Hello $name\nHello $name\nHello $name",
-        color = Color.Blue,
-        fontSize = 15.sp,
-        fontWeight = FontWeight.Bold,
-        fontFamily = FontFamily.SansSerif,
-        letterSpacing = 2.sp,
-        maxLines = 2,
-        textDecoration = TextDecoration.Underline,
-        textAlign = TextAlign.Start,
-    )
+    /** 01. 텍스트 */
+    @Composable
+    fun Greeting(name: String) {
+        Text(
+            modifier = Modifier.width(300.dp),
+            text = "Hello $name\nHello $name\nHello $name",
+            color = Color.Blue,
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.SansSerif,
+            letterSpacing = 2.sp,
+            maxLines = 2,
+            textDecoration = TextDecoration.Underline,
+            textAlign = TextAlign.Start,
+        )
+    }
+
+    /** 02. 버튼 */
+    @Composable
+    fun ButtonExample(onButtonClicked: () -> Unit) {
+        Button(
+            onClick = onButtonClicked,
+            enabled = true,
+            border = BorderStroke(3.dp, Color.Magenta),
+            shape = CircleShape,
+            contentPadding = PaddingValues(horizontal = 10.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Send,
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Text(text = "Send")
+        }
+    }
 }
