b887354d3b123b520a56e91840215503b3e73c4c
이경은 <dientes8520@gmail.com>
2023-11-27 21:25:10 +0900

    learn: Slot Api

--- app/src/main/java/com/hamcoding/hellocompose/MainActivity.kt
+++ app/src/main/java/com/hamcoding/hellocompose/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -43,6 +44,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -69,7 +71,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HelloComposeTheme {
-                TopBarEx()
+                SlotEx()
             }
         }
     }
@@ -78,7 +80,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun ComposePreview() {
         HelloComposeTheme {
-            TopBarEx()
+            SlotEx()
         }
     }
 
@@ -410,5 +412,50 @@ class MainActivity : ComponentActivity() {
                 }
             }
         )
+    }
+
+    @Composable
+    fun CheckBoxWithSlot(
+        checked: Boolean,
+        onCheckedChanged: () -> Unit,
+        content: @Composable RowScope.() -> Unit
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.clickable {
+                onCheckedChanged()
+            }
+        ) {
+            Checkbox(
+                checked = checked,
+                onCheckedChange = {
+                    onCheckedChanged()
+                }
+            )
+            content()
+        }
+    }
+
+    @Composable
+    fun SlotEx() {
+        var checked1 by remember { mutableStateOf(false) }
+        var checked2 by remember { mutableStateOf(false) }
+
+        Column {
+            CheckBoxWithSlot(checked = checked1, onCheckedChanged = {
+                checked1 = !checked1
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "추가"
+                )
+                Text(text = "예시")
+            }
+            CheckBoxWithSlot(checked = checked2, onCheckedChanged = {
+                checked2 = !checked2
+            }) {
+                Text(text = "뿡")
+            }
+        }
     }
 }
